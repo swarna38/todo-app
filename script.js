cc05cd85c23bafa2b5a90f5ff0b22bfc4d812e46
swarna38 <swarna.sutradhar.info@gamil.com>
2025-07-29 13:59:21 +0600

    update done

--- script.js
+++ script.js
@@ -6,32 +6,8 @@ const todoList = document.getElementById('todo-list');
 // create an  Array to gets todos
 let todos = [];
 
-function renderTodos(){
-
-    //clear existing list
-    todoList.innerHTML= " ";
-
-    todos.forEach((todo, index) => {
-        //create li
-        const li = document.createElement('li');
-        li.className = 'list-group-item d-flex justify-content-between align-items-center';
-        li.textContent = todo ;
-
-        // Create Delete button
-        const deleteButton = document.createElement("button");
-        deleteButton.className = 'btn btn-danger btn-sm';
-        deleteButton.textContent = "delete";
-
-        //delete handler
-        deleteButton.addEventListener('click', ()=>{
-            deleteTodo(index);
-        });
-
-        li.appendChild(deleteButton);
-        todoList.appendChild(li);
-
-    });
-}
+let isEditing = false;
+let editingIndex = null;
 
 
 //add todo
@@ -46,6 +22,63 @@ function deleteTodo(index){
     renderTodos();
 }
 
+//updated todo
+function updateTodo(index, newTodo){
+    todos[index] = newTodo;
+    renderTodos();
+}
+
+//reset 
+function resetForm() {
+    todoInput.value = '';
+    isEditing = false;
+    editingIndex = null;
+    todoForm.querySelector('button').textContent = 'Add Todo';
+}
+
+
+function renderTodos(){
+
+    //clear existing list
+    todoList.innerHTML= "";
+
+    todos.forEach((todo, index) => {
+        //create li
+        const li = document.createElement('li');
+        li.className = 'list-group-item d-flex justify-content-between align-items-center';
+
+        //create span for li text
+        const span = document.createElement('span');
+        span.textContent = todo;
+        span.style.cursor = 'pointer';
+
+        //update work
+        span.addEventListener('click', () =>{
+            isEditing = true;
+            editingIndex = index;
+            todoInput.value = todo;
+            todoForm.querySelector('button').textContent = 'Update todo';
+        })
+
+        // Create Delete button
+        const deleteButton = document.createElement("button");
+        deleteButton.className = 'btn btn-danger btn-sm';
+        deleteButton.textContent = "delete";
+
+        //delete handler
+        deleteButton.addEventListener('click', ()=>{
+            deleteTodo(index);
+        });
+
+        li.appendChild(span);
+        li.appendChild(deleteButton);
+        todoList.appendChild(li);
+
+    });
+}
+
+
+
 //from handler
 todoForm.addEventListener('submit', (e) =>{
     e.preventDefault();
@@ -53,9 +86,15 @@ todoForm.addEventListener('submit', (e) =>{
     //add new todo
     const newTodo = todoInput.value.trim();
 
+    //check input not empty
     if(newTodo){
-        addTodo(newTodo);
-        //clear inputfield
-        todoInput.value = "";
+
+        //check if editing mode
+        if(isEditing){
+            updateTodo(editingIndex, newTodo);
+        }else{
+            addTodo(newTodo);
+        }
     }
+    resetForm();
 })
